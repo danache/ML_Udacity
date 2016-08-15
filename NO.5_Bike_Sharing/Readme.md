43e49f99d0f5a53e4e2b95157c46ab843a666384
danache <danache@126.com>
2016-08-15 15:25:14 +0800

    All completed

--- NO.5_Bike_Sharing/Readme.md
+++ NO.5_Bike_Sharing/Readme.md
@@ -1,11 +1,17 @@
-﻿# Readme
+# Kaggle Bike Rent Project
+
+> To solve Bike Sharing Demand project , see detail description in https://www.kaggle.com/c/bike-sharing-demand
+
+This project is made to solve Kaggle Bike Rent Project in Kaggle.
+I first take data procession to transform feature and remove it.Then I choose random forest model , decision tree model, SVM to fit data.Then I use grid search to find better parameters for random forest.The final results rank top 20% in kaggle.
 
 
----
+### Environment
+The peojected is written in python3.5。
+Librarys：sklearn, pandas, numpy, IPython.display。
 
-项目语言python3.5
-依赖库：sklearn, pandas, numpy, IPython.display.
+### Usage
+You can use python compiler to execute bike.py to get result.
 
-
-
-
+### Meta
+danache - danache@126.com
--- NO.5_Bike_Sharing/bike.py
+++ NO.5_Bike_Sharing/bike.py
@@ -12,8 +12,6 @@ from sklearn.cross_validation import train_test_split
 from sklearn import tree
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
-
-
 import time
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
@@ -25,7 +23,6 @@ from sklearn.cross_validation  import cross_val_score
 from sklearn import linear_model
 from sklearn import svm
 from sklearn.ensemble import GradientBoostingRegressor
-
 import pandas as pd
 from pandas import Series,DataFrame
 import numpy as np
@@ -33,22 +30,23 @@ import matplotlib.pyplot as plt
 import datetime
 from IPython.display import display
 
+
+# Load training and test datasets
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
-
+# make a copy of the data
 train_orig = train.copy()
 test_orig = test.copy()
-
+# check variables
 train.head()
 test.head()
-print(train.head())
-print(len(train))
-print(len(test))
-print(len(train.columns))
-
-
+train.head()
+len(train)
+len(test)
+len(train.columns)
+# drop unnecessary feature
 train = train.drop(labels=["casual", "registered"], axis=1)
-
+#get useful features from "datetime"
 train['year'] = train['datetime'].str.extract("^(.{4})")
 test['year'] = test['datetime'].str.extract("^(.{4})")
 
@@ -61,25 +59,28 @@ test['day'] = test['datetime'].str.extract("(.{2}) ")
 train['time'] = train['datetime'].str.extract(" (.{2})")
 test['time'] = test['datetime'].str.extract(" (.{2})")
 
-train[['year', 'month', 'day', 'time']] = train[['year', 'month', 'day', 'time']].astype(int)
-test[['year', 'month', 'day', 'time']] = test[['year', 'month', 'day', 'time']].astype(int)
+train[['year', 'month', 'day', 'time']] = train\
+    [['year', 'month', 'day', 'time']].astype(int)
+test[['year', 'month', 'day', 'time']] = test\
+    [['year', 'month', 'day', 'time']].astype(int)
 
-train['dayOfWeek'] = train.apply(lambda x: datetime.date(x['year'], x['month'], x['day']).weekday(), axis=1)
-test['dayOfWeek'] = test.apply(lambda x: datetime.date(x['year'], x['month'], x['day']).weekday(), axis=1)
+train['dayOfWeek'] = train.apply(lambda x: datetime.date\
+    (x['year'], x['month'], x['day']).weekday(), axis=1)
+test['dayOfWeek'] = test.apply(lambda x: datetime.date\
+    (x['year'], x['month'], x['day']).weekday(), axis=1)
 
 train = train.drop(labels=["datetime"], axis=1)
 test = test.drop(labels=["datetime"], axis=1)
 
-
+#convert ordinal categorical variables into multiple dummy variables
 train['season'].value_counts()
 train = train.join(pd.get_dummies(train.season, prefix='season'))
 test = test.join(pd.get_dummies(test.season, prefix='season'))
 
-
 train = train.drop(labels=["season"], axis=1)
 test = test.drop(labels=["season"], axis=1)
 
-
+#convert categorical variables into multiple dummy variables
 train['weather'].value_counts()
 train = train.join(pd.get_dummies(train.weather, prefix='weather'))
 test = test.join(pd.get_dummies(test.weather, prefix='weather'))
@@ -89,15 +90,15 @@ train = train.drop(labels=["weather"], axis=1)
 test = test.drop(labels=["weather"], axis=1)
 
 train.corr()
-
+#drop highly correlated predictors "atemp"
 train = train.drop(labels=["atemp"], axis=1)
 test = test.drop(labels=["atemp"], axis=1)
 
-
+#get target feature
 target = train['count'].values
 train = train.drop(labels=["count"], axis=1)
 
-
+#Predictions in log space
 target = np.log(target)
 
 
@@ -107,12 +108,11 @@ def bs_fit_and_save(clf, l_train, l_target, l_test, filename):
     clf.fit (l_train, l_target)
 
     predict_train = clf.predict(l_train)
-
+     # The mean square error
     print('Variance score: %.2f' % clf.score(l_train, l_target))
-
     predict_test = clf.predict(l_test)
     predict_test = np.exp(predict_test)
-
+    #out put to *.csv
     output = test_orig['datetime']
     output = pd.DataFrame(output)
     predict = pd.DataFrame(predict_test)
@@ -120,7 +120,7 @@ def bs_fit_and_save(clf, l_train, l_target, l_test, filename):
     output.columns = ['datetime', 'count']
     output.to_csv(filename + ".csv", index=False)
     return clf
-
+#cross validation
 def getFile(clf,l_test, filename):
 
     predict_test = clf.predict(l_test)
@@ -137,23 +137,30 @@ def getFile(clf,l_test, filename):
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
-
 from sklearn.cross_validation import ShuffleSplit
 from  sklearn.grid_search import GridSearchCV
+from sklearn import  linear_model
 
 
-#clf = RandomForestRegressor(random_state=0, n_estimators=100)
-#clf = bs_fit_and_save(clf, train, target, test, "output_RandomForest")
-clf = SVR()
+#clf = RandomForestRegressor()
+#clf = bs_fit_and_save(clf, train, target, test, "RandomForest")
+#clf = linear_model.BayesianRidge()
+#clf = bs_fit_and_save(clf, train, target, test, "linear")
+
+clf = RandomForestRegressor()
+
+#grid search
+cv_sets = ShuffleSplit(train.shape[0], n_iter = 10, \
+                       test_size = 0.20, random_state = 0)
 
 
-cv_sets = ShuffleSplit(train.shape[0], n_iter = 10, test_size = 0.20, random_state = 0)
-
-
-params = {'C' : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],'epsilon':[0.2,0.3,0.4,0.5,0.6,0.7,0.8]}
+params = {'n_estimators' : list(range(50,131,10)),\
+          'max_features' : ["auto","log2"]}
 
 grid = GridSearchCV(clf, params,cv = cv_sets )
 
 grid = grid.fit(train, target)
 clf = grid.best_estimator_
-clf = getFile(clf,test, "output_SVRCross")
+grid.best_params_
+grid.best_score_
+clf = getFile(clf,test, "output_randomForest++++++")
